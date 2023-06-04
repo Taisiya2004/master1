1ddf5fcb2f7643a5ac1bac92acf29a2ac2e8a4db
Taisiya <116912343+Taisiya2004@users.noreply.github.com>
2023-06-04 12:16:47 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,133 +1,90 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <algorithm>
+#include <fstream>
+#include <string>
 #include <sstream>
+#include <algorithm>
 
-using namespace std;
+using namespace {
+    const std::string engineer_ = "инженер";
+    const std::string chairman_ = "председатель";
+}
 
-// Класс для хранения информации о сотруднике
-class Employee {
+class Person {
 private:
-    string name;
-    int birthYear;
-    string position;
-    string unit;
-
+    std::string Name;
+    int Year;
+    std::string Position;
+    std::string Group;
 public:
-    // Конструктор
-    Employee(const string& name,const int& birthYear, const string& position, const string& unit) :
-        name(name), birthYear(birthYear), position(position), unit(unit) {}
+    Person(const std::string& name, int year, const std::string& position, const std::string& group) :
+        Name(name), Year(year), Position(position), Group(group) {}
 
-    // Геттеры
-    string getName() const {
-        return name;
-    }
-    int getBirthYear() const {
-        return birthYear;
-    }
-    string getPosition() const {
-        return position;
-    }
-    string getUnit() const {
-        return unit;
-    }
-
-    // Метод для вывода информации о сотруднике
+    std::string GetName() const { return Name; }
+    int GetYear() const { return Year; }
+    std::string GetPosition() const  { return Position; }
+    std::string GetGroup() const { return Group; }
     void print() const {
-        cout << name << " " << birthYear << ", " << position << ", " << unit << endl;
+        std::cout << Name << ", " << Year << ", " << Position << ", " << Group << std::endl;
     }
 };
 
-// Класс для хранения БД сотрудников
-class EmployeeDB {
+class DB {
 private:
-    vector<Employee*> employees;
-
+    std::vector<Person> Employees;
 public:
-    // Конструктор по умолчанию
-    EmployeeDB() {}
-
-    // Деструктор
-    ~EmployeeDB() {
-        for (auto emp : employees) {
-            delete emp;
-        }
+    void AddPerson(const std::string& name, int year, const std::string& position, const std::string& group) {
+        Employees.emplace_back(Person(name, year, position, group));
     }
-
-    // Метод для добавления сотрудника в БД
-    void addEmployee(Employee* emp) {
-        employees.push_back(emp);
-    }
-
-    // Метод для вывода информации о всех инженерах
-    void printEngineers() const {
-        cout << "Engineers:" << endl;
-        for (auto emp : employees) {
-            if (emp->getPosition() == "инженер") {
-                emp->print();
+    void PrintEngineer() const {
+        for (const auto& p : Employees) {
+            if (p.GetPosition() == engineer_) {
+                p.print();
             }
         }
     }
 
-    // Метод для вывода информации о всех сотрудниках, не являющихся председателями, в порядке возрастания года рождения
-    void printNonChairmen() const {
-        // Создаем вектор для хранения сотрудников, не являющихся председателями
-        vector<Employee*> nonChairmen;
-        for (auto emp : employees) {
-            if (emp->getPosition() != "председатель") {
-                nonChairmen.push_back(emp);
+    void PrintNoPredsedatel() {
+        std::sort(Employees.begin(), Employees.end(), [](const Person& p1, const Person& p2) {
+            return p1.GetYear() < p2.GetYear();
+        });
+        for (const auto& p : Employees) {
+            if (p.GetPosition() != chairman_) {
+                p.print();
             }
         }
+    }
 
-        // Сортируем сотрудников, не являющихся председателями, по возрастанию года рождения
-        sort(nonChairmen.begin(), nonChairmen.end(), [](Employee* a, Employee* b) {
-            return a->getBirthYear() < b->getBirthYear();
-            });
-
-        // Выводим информацию о сотрудниках, не являющихся председателями, в порядке возрастания года рождения
-        cout << "Non-chairmen:" << endl;
-        for (auto emp : nonChairmen) {
-            emp->print();
-        }
+    ~DB() {
+        Employees.clear();
     }
 };
 
-int main() {
+int main()
+{
     setlocale(LC_ALL, "rus");
-    // Открываем файл
-    ifstream file("Info.txt");
-   
-
-    // Создаем объект класса EmployeeDB
-    EmployeeDB db;
-
-    // Считываем данные из файла и добавляем сотрудников в БД
-    string line;
-    if (!file.is_open()) {
+    DB db;
+    std::ifstream fl("Info.txt");
+    if (!fl.is_open()) {
         std::cout << "Error opening file!" << std::endl;
     }
     else {
         std::string line;
         std::string name, position, group;
         int year;
-        while (std::getline(file, line)) {
+        while (std::getline(fl, line)) {
             std::stringstream ss(line);
             std::getline(ss, name, ',');
             ss >> year >> std::ws;
             std::getline(ss, position, ',');
             std::getline(ss >> std::ws, group);
-            db.addEmployee(new Employee(name, year, position, group));
+            db.AddPerson(name, year, position, group);
         }
     }
-    // Закрываем файл
-    file.close();
 
-    // Выводим информацию о всех инженерах
-    db.printEngineers();
-
-    // Выводим информацию о всех сотрудниках, не являющихся председателями, в порядке возрастания года рождения
-    db.printNonChairmen();
-
+    db.PrintEnergineer();
+    std::cout << "------------------------------------" << std::endl;
+    db.PrintNoPredsedatel();
+    fl.close();
     return 0;
 }
